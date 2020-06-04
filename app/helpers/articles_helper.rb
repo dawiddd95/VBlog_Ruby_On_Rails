ca66af12088108edf4de0fb68f5b1058c89a4782
dawiddd95 <dawlyc@wp.pl>
2020-06-04 18:29:12 +0200

    home index view updated

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,18 @@
 module ArticlesHelper
+   def format_date(date)
+      month = date.month
+      day = date.wday
+      year = date.year
+      month_name = Date::MONTHNAMES[month]
+
+      return "#{month_name} #{day}, #{date.year}"
+   end
+
+   def cut_content(text)
+      return "#{text[0..220]} #{text.length > 220 ? "..." : ""}"
+   end
+
+   def cut_title(text)
+      return "#{text[0..60]} #{text.length > 60 ? "..." : ""}"
+   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,2 @@
 module HomeHelper
-   def format_date(date)
-      month = date.month
-      day = date.wday
-      year = date.year
-      month_name = Date::MONTHNAMES[month]
-
-      return "#{month_name} #{day}, #{date.year}"
-   end
 end
